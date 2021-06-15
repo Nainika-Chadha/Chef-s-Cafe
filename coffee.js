4625eda5a7d68aff5e8c6029bb0a589a6dca3511
Nainika-Chadha <72744699+Nainika-Chadha@users.noreply.github.com>
2021-06-15 19:51:22 +0530

    Add files via upload

--- coffee.js
+++ coffee.js
@@ -17,7 +17,7 @@ menu_list_array.push(item);
 menu_list_array.sort();
 htmldata="<section class='cards'>"
 for( var i=0; i<menu_list_array.length;i++){
-htmldata=htmldata+'<div class="card">'+'<img src="images/ci.webp">'+menu_list_array[i]+'</div>'
+htmldata=htmldata+'<div class="card">'+'<img src="coffee-around-the-world.png">'+menu_list_array[i]+'</div>'
 }
 htmldata=htmldata+"<section>"
 document.getElementById('display_addedmenu').innerHTML=htmldata;
